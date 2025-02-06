224496e0f40d871ec7a1f6a3b39adb61937af016
Om Gawali <omagawali1701@gmail.com>
2025-02-06 23:38:12 +0530

    STM32_code

---
+++ STM32/Non_RTOS_Project/Core/Inc/struct.h
@@ -0,0 +1,28 @@
+/*
+ * struct.h
+ *
+ *  Created on: Feb 4, 2025
+ *      Author: ADMIN
+ */
+
+#ifndef INC_STRUCT_H_
+#define INC_STRUCT_H_
+
+typedef struct SENSOR{
+	float Temperature;
+	float Pressure;
+	float Humidity;
+	uint16_t ADC_Value;
+	float mq135_voltage;
+	float Rs;
+	float Co2;
+	float NH3;
+	float Ethanol;
+	float AQI;
+}Sensordata_t;
+
+extern Sensordata_t SD;//Structure Variable to store different sensor values
+
+
+
+#endif /* INC_STRUCT_H_ */
