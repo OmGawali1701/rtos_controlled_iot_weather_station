83347d306bf68bc643d65648ca89ea65ca37f957
Tulsiram sagar <tulsisagar1998@gmail.com>
2025-02-09 19:12:10 +0530

    final_project

---
+++ RTOS_Controlled_IoT_Weather_Station/RTOS_Controlled_IoT_Weather_Station/Debug/Middleware/SEGGER/SEGGER/SEGGER_SYSVIEW.d
@@ -0,0 +1,18 @@
+Middleware/SEGGER/SEGGER/SEGGER_SYSVIEW.o: \
+ ../Middleware/SEGGER/SEGGER/SEGGER_SYSVIEW.c \
+ ../Middleware/SEGGER/SEGGER/SEGGER_SYSVIEW_Int.h \
+ ../Middleware/SEGGER/SEGGER/SEGGER_SYSVIEW.h \
+ ../Middleware/SEGGER/SEGGER/SEGGER.h \
+ C:/Users/ADMIN/STM32CubeIDE/workspace_1.16.1/RTOS_Controlled_IoT_Weather_Station/Middleware/SEGGER/Config/Global.h \
+ ../Middleware/SEGGER/SEGGER/SEGGER_SYSVIEW_ConfDefaults.h \
+ C:/Users/ADMIN/STM32CubeIDE/workspace_1.16.1/RTOS_Controlled_IoT_Weather_Station/Middleware/SEGGER/Config/SEGGER_SYSVIEW_Conf.h \
+ ../Middleware/SEGGER/SEGGER/SEGGER_RTT_Conf.h \
+ ../Middleware/SEGGER/SEGGER/SEGGER_RTT.h
+../Middleware/SEGGER/SEGGER/SEGGER_SYSVIEW_Int.h:
+../Middleware/SEGGER/SEGGER/SEGGER_SYSVIEW.h:
+../Middleware/SEGGER/SEGGER/SEGGER.h:
+C:/Users/ADMIN/STM32CubeIDE/workspace_1.16.1/RTOS_Controlled_IoT_Weather_Station/Middleware/SEGGER/Config/Global.h:
+../Middleware/SEGGER/SEGGER/SEGGER_SYSVIEW_ConfDefaults.h:
+C:/Users/ADMIN/STM32CubeIDE/workspace_1.16.1/RTOS_Controlled_IoT_Weather_Station/Middleware/SEGGER/Config/SEGGER_SYSVIEW_Conf.h:
+../Middleware/SEGGER/SEGGER/SEGGER_RTT_Conf.h:
+../Middleware/SEGGER/SEGGER/SEGGER_RTT.h:
